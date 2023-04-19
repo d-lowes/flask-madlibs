c4ac0fb0dfdde3822197af0814a826dc48b8b7de
John Chong <johnchong.99.17@gmail.com>
2023-04-19 18:33:22 -0500

    finish initial work

--- app.py
+++ app.py
@@ -7,3 +7,26 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get('/')
+def get_base():
+    """
+    display madlibs prompt at start of website
+    with required parts to generate Madlib
+    """
+
+    return render_template("questions.html", prompts = silly_story.prompts)
+
+
+@app.get('/results')
+def get_results():
+    """
+    Generate Madlib from inputted prompts
+    """
+
+    print(request.args)
+    generated_madlib = silly_story.get_result_text(request.args)
+
+
+    return render_template("results.html", generated_story=generated_madlib)
